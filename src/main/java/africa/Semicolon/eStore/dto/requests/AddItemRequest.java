265bae57c38558894652dd86785f257f5fcc4bd9
Orisakwe Nwokocha <orisakwenwokocha1@gmail.com>
2024-04-09 09:24:03 +0100

    eStore 1.1 completed and deployed

--- src/main/java/africa/Semicolon/eStore/dto/requests/AddItemRequest.java
+++ src/main/java/africa/Semicolon/eStore/dto/requests/AddItemRequest.java
@@ -1,5 +1,6 @@
 package africa.Semicolon.eStore.dto.requests;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -10,5 +11,6 @@ public final class AddItemRequest {
     @NotNull(message = "Product id cannot be null")
     private String productId;
     @NotNull(message = "Quantity cannot be null")
+    @Min(value = 1, message = "Quantity must be greater than or equal to 1")
     private int quantityOfProduct;
 }
--- src/main/java/africa/Semicolon/eStore/dto/requests/AddProductRequest.java
+++ src/main/java/africa/Semicolon/eStore/dto/requests/AddProductRequest.java
@@ -1,5 +1,6 @@
 package africa.Semicolon.eStore.dto.requests;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -11,9 +12,11 @@ public final class AddProductRequest {
     @NotNull(message = "Product name cannot be null")
     private String name;
     @NotNull(message = "Price cannot be null")
+    @Min(value = 1, message = "Price must be greater than or equal to 1")
     private double price;
     @NotNull(message = "Category cannot be null")
     private String category;
     @NotNull(message = "Quantity cannot be null")
+    @Min(value = 1, message = "Quantity must be greater than or equal to 1")
     private int quantity;
 }
